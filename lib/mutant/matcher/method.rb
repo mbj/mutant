586284544a7615d04d0e9900694c039ea68bd7d3
Markus Schirp <mbj@seonic.net>
2012-08-29 12:44:03 +0200

    Add equalization on identification string for matchers

--- lib/mutant/matcher/method.rb
+++ lib/mutant/matcher/method.rb
@@ -2,7 +2,7 @@ module Mutant
   class Matcher
     # Matcher to find AST for method
     class Method < self
-      include Immutable
+      include Immutable, Equalizer.new(:identification)
 
       # Parse a method string into filter
       #
