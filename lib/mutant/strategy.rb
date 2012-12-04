8c3cc4a18c3706b5ad364efae9b1f51a35a4c84a
Markus Schirp <mbj@seonic.net>
2012-12-04 18:23:56 +0100

    Add rspec world preserve hack

--- lib/mutant/strategy.rb
+++ lib/mutant/strategy.rb
@@ -29,6 +29,29 @@ module Mutant
     end
 
     class Rspec < self
+
+      def self.original_world
+        @original_world ||=
+          begin
+            require './spec/spec_helper'
+            ::RSpec.world
+          end
+      end
+
+      def self.prepare_world
+        ::RSpec.instance_variable_set(:@world, original_world)
+        cleanup_world
+      end
+
+      def self.cleanup_world
+        ::RSpec::Core::ExampleGroup.children.clear
+        ::RSpec::Core::ExampleGroup.constants.each do |name|
+          if name =~ /^Nested/
+            RSpec::Core::ExampleGroup.send(:remove_const, name)
+          end
+        end
+      end
+
       KILLER = Killer::Rspec
 
       class DM2 < self
