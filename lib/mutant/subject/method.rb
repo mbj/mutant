d34d8565d2ca1f667485bd389277ac676bd248e7
Markus Schirp <mbj@seonic.net>
2013-01-13 22:25:49 +0100

    Add method specific subject subclass

---
+++ lib/mutant/subject/method.rb
@@ -0,0 +1,82 @@
+module Mutant
+  class Subject
+    # Abstract base class for method subjects
+    class Method < self
+
+      # Test if method is public
+      #
+      # @return [true]
+      #   if method is public
+      #
+      # @return [false]
+      #   otherwise
+      #
+      # @api private
+      #
+      abstract_method :public?
+
+      # Instance method subjects
+      class Instance < self
+
+        # Test if method is public
+        #
+        # @return [true]
+        #   if method is public
+        #
+        # @return [false]
+        #   otherwise
+        #
+        # @api private
+        #
+        def public?
+          scope.public_method_defined?(method_name)
+        end
+        memoize :public?
+
+      private
+
+        # Return subtype identifier
+        #
+        # @return [String]
+        #
+        # @api private
+        #
+        def subtype
+          "#{context.name}##{node.name}"
+        end
+
+      end
+
+      # Singleton method subjects
+      class Singleton < self 
+
+        # Test if method is public
+        #
+        # @return [true]
+        #   if method is public
+        #
+        # @return [false]
+        #   otherwise
+        #
+        # @api private
+        #
+        def public?
+          scope.singleton_class.public_method_defined?(method_name)
+        end
+        memoize :public?
+
+      private
+
+        # Return subtype identifier
+        #
+        # @return [String]
+        #
+        # @api private
+        #
+        def subtype
+          "#{context.name}.#{node.name}"
+        end
+      end
+    end
+  end
+end
