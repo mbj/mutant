a7226482a3ba6631e839d89cf9c04bfa23611f75
Markus Schirp <mbj@seonic.net>
2013-02-24 18:35:43 +0100

    Fix "idempotent method" shared example helper
    rspec-2.13.0 changed the way the subject is exposed.
    We do not have access to the block anymore.
    Need to delete memoized value from the __memoized hash to reset subject.

--- spec/shared/idempotent_method_behavior.rb
+++ spec/shared/idempotent_method_behavior.rb
@@ -2,6 +2,8 @@
 
 shared_examples_for 'an idempotent method' do
   it 'is idempotent' do
-    should equal(instance_eval(&self.class.subject))
+    first = subject
+    __memoized.delete(:subject)
+    should equal(first)
   end
 end
