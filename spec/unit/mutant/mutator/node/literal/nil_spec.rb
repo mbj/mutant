1519bfa3877f51eba478b8abb39f999ef5eb4a1f
Markus Schirp <mbj@schirp-dso.com>
2013-06-12 19:24:41 +0200

    Fix literal nil mutator specs

--- spec/unit/mutant/mutator/node/literal/nil_spec.rb
+++ spec/unit/mutant/mutator/node/literal/nil_spec.rb
@@ -4,9 +4,7 @@ describe Mutant::Mutator::Node::Literal, 'nil' do
   let(:source) { 'nil' }
 
   let(:mutations) do
-    mutations = []
-
-    mutations << 'Object.new'
+    [ '::Object.new' ]
   end
 
   it_should_behave_like 'a mutator'
