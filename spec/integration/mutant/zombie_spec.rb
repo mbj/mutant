5efd17d4e3fc88c6487811329edf862a88c8c1c0
Markus Schirp <mbj@schirp-dso.com>
2013-06-14 20:28:18 +0200

    Mark zombie specs as pending

--- spec/integration/mutant/zombie_spec.rb
+++ spec/integration/mutant/zombie_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Mutant, 'as a zombie' do
-  specify 'allows to create zombie from mutant' do
+  pending 'allows to create zombie from mutant' do
     Zombie.setup
     Zombie::Runner
   end
