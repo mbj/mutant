0451b940b304ee14110c0f27146a1bc198001c0a
Markus Schirp <mbj@schirp-dso.com>
2013-07-02 17:48:48 +0200

    Kill mutations in Mutant.singleton_subclass_instance

---
+++ spec/unit/mutant/class_methods/singleton_subclass_instance_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe Mutant, '.singleton_subclass_instance' do
+  let(:object) { described_class }
+
+  subject { object.singleton_subclass_instance(name, superclass, &block) }
+
+  before do
+    subject
+  end
+
+  let(:name)       { 'Test'                }
+  let(:block)      { proc { def foo; end } }
+  let(:superclass) { Class.new             }
+
+  let(:generated) { superclass.const_get(:Test) }
+
+  it_should_behave_like 'a command method'
+
+  it 'sets expected name' do
+    name = generated.class.name
+    name.should eql("::#{self.name}")
+    name.should be_frozen
+  end
+
+  it 'stores instance of subclass' do
+    generated.should be_kind_of(superclass)
+  end
+
+  it 'evaluates the context of proc inside subclass' do
+    generated.should respond_to(:foo)
+  end
+
+  it 'generates nice #inspect' do
+    inspect = generated.inspect
+    inspect.should eql("::#{self.name}")
+    inspect.should be_frozen
+  end
+end
