ec9da5da97adff6bfacc4b591411ece01945a31e
Dan Kubb <dan.kubb@gmail.com>
2013-07-22 09:08:01 -0700

    Add Mutant::Mutator::Node::Variable
    * Rename lvar mutator to handle all variable usage

--- lib/mutant/mutator/node/generic.rb
+++ lib/mutant/mutator/node/generic.rb
@@ -11,10 +11,10 @@ module Mutant
         # your contribution is that close!
         handle(
           :zsuper, :not, :or, :and, :defined,
-          :next, :break, :match, :gvar, :cvar, :ensure,
+          :next, :break, :match, :ensure,
           :dstr, :dsym, :yield, :rescue, :redo, :defined?,
           :const, :blockarg, :block_pass, :op_asgn, :and_asgn,
-          :regopt, :ivar, :restarg, :casgn, :resbody, :retry, :arg_expr,
+          :regopt, :restarg, :casgn, :resbody, :retry, :arg_expr,
           :kwrestarg, :kwoptarg, :kwarg, :undef, :module, :cbase, :empty,
           :alias, :for, :xstr, :back_ref, :nth_ref, :class,
           :sclass, :match_with_lvasgn, :match_current_line, :or_asgn, :kwbegin
---
+++ spec/unit/mutant/mutator/node/cvar/mutation_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Mutant::Mutator, 'cvar' do
+
+  before do
+    Mutant::Random.stub(:hex_string => 'random')
+  end
+
+  let(:source) { '@@a = nil; @@a' }
+
+  let(:mutations) do
+    mutants = []
+    mutants << '@@a = nil; nil'
+    mutants << '@@a = nil'
+    mutants << '@@a'
+    mutants << '@@a = ::Object.new; @@a'
+    mutants << '@@srandom = nil; @@a'
+  end
+
+  it_should_behave_like 'a mutator'
+end
