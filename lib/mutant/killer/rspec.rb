c923e1b5ed336b6a25a47be3f3b6e9e3f6056f28
Jesse Kempf <jesse.kempf@opower.com>
2013-07-23 16:42:05 -0700

    Surface the output of rspec failures when killing noop mutants.

--- lib/mutant/killer/rspec.rb
+++ lib/mutant/killer/rspec.rb
@@ -18,9 +18,21 @@ module Mutant
       def run
         mutation.insert
         # TODO: replace with real streams from configuration
+        # Note: we assume the only interesting output from a failed rspec run is stderr.
         require 'stringio'
-        null = StringIO.new
-        !::RSpec::Core::Runner.run(command_line_arguments, null, null).zero?
+        rspec_err = StringIO.new
+
+        killed = !::RSpec::Core::Runner.run(command_line_arguments, nil, rspec_err).zero?
+
+        if killed and mutation.should_survive?
+          rspec_err.rewind
+
+          puts "#{mutation.class} test failed."
+          puts 'RSpec stderr:'
+          puts rspec_err.read
+        end
+
+        killed
       end
 
       # Return command line arguments
