fc313945a12d52960ea9000f86a45b5e997367f2
Dan Kubb <dan.kubb@gmail.com>
2013-07-23 23:16:43 -0700

    Remove :not node
    * The :not node is not used in Ruby 1.9+

--- lib/mutant/mutator/node/generic.rb
+++ lib/mutant/mutator/node/generic.rb
@@ -8,7 +8,7 @@ module Mutant
         # These nodes still need a dedicated mutator,
         # your contribution is that close!
         handle(
-          :not, :defined,
+          :defined,
           :next, :break, :match, :ensure,
           :dstr, :dsym, :yield, :rescue, :redo, :defined?,
           :blockarg, :block_pass, :op_asgn, :and_asgn,
