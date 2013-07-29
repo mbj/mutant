ab3ec615ab8bf23b97e26596fc442a572fe2d40a
Dan Kubb <dan.kubb@gmail.com>
2013-07-29 16:16:50 -0700

    Fix failing spec caused by bug fix in unparser

--- spec/unit/mutant/mutator/node/next/mutation_spec.rb
+++ spec/unit/mutant/mutator/node/next/mutation_spec.rb
@@ -3,8 +3,13 @@
 require 'spec_helper'
 
 describe Mutant::Mutator::Node::Generic, 'next' do
-  let(:source)    { 'next true' }
-  let(:mutations) { []          }
+  let(:source)  { 'next true' }
+
+  let(:mutations) do
+    mutations = []
+    mutations << 'next false'
+    mutations << 'next nil'
+  end
 
   it_should_behave_like 'a mutator'
 end
