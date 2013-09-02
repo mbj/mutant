f2f097e5e91139015a59cc01bbeb3dd2abe27304
Markus Schirp <mbj@schirp-dso.com>
2013-09-02 22:02:51 +0200

    Return self on noop command methods

--- lib/mutant/strategy.rb
+++ lib/mutant/strategy.rb
@@ -13,6 +13,7 @@ module Mutant
     # @api private
     #
     def setup
+      self
     end
 
     # Perform strategy teardown
@@ -22,6 +23,7 @@ module Mutant
     # @api private
     #
     def teardown
+      self
     end
 
     # Kill mutation
