c121e8b400fbbb74684c9bb17a9695152c8ed76f
Markus Schirp <mbj@schirp-dso.com>
2013-09-13 23:54:11 +0200

    Whitespace

--- spec/integration/mutant/rspec_spec.rb
+++ spec/integration/mutant/rspec_spec.rb
@@ -18,7 +18,6 @@ describe Mutant, 'rspec integration' do
     Kernel.system('bundle exec mutant -I lib --require test_app --rspec ::TestApp::Literal#string --ignore-subject ::TestApp::Literal#uncovered_string').should be(true)
   end
 
-
   specify 'fails to kill mutations when they are not covered' do
     cli = 'bundle exec mutant -I lib --require test_app --rspec ::TestApp::Literal#uncovered_string'
     Kernel.system(cli).should be(false)
