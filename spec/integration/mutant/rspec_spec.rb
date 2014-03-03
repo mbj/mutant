30092dd9566d80f2df4e645dc716fbff9b50e7f0
Andrey Savchenko <andrey@aejis.eu>
2014-03-03 20:00:57 +0200

    Test both rspec 2 and 3

--- spec/integration/mutant/rspec_spec.rb
+++ spec/integration/mutant/rspec_spec.rb
@@ -4,35 +4,52 @@ require 'spec_helper'
 
 describe Mutant, 'rspec integration' do
 
-  around do |example|
-    Dir.chdir(TestApp.root) do
-      example.run
+  let(:base_cmd) { "bundle exec mutant -I lib --require test_app --use rspec" }
+
+  shared_examples_for 'rspec integration' do
+    around do |example|
+      Bundler.with_clean_env do
+        Dir.chdir(TestApp.root) do
+          Kernel.system("bundle install --gemfile=#{gemfile}")
+          example.run
+        end
+      end
+    end
+
+    specify 'it allows to kill mutations' do
+      expect(Kernel.system("#{base_cmd} ::TestApp::Literal#string")).to be(true)
+    end
+
+    specify 'it allows to exclude mutations' do
+      cli = <<-CMD.split("\n").join(' ')
+        #{base_cmd}
+        ::TestApp::Literal#string
+        ::TestApp::Literal#uncovered_string
+          --ignore-subject ::TestApp::Literal#uncovered_string
+      CMD
+      expect(Kernel.system(cli)).to be(true)
+    end
+
+    specify 'fails to kill mutations when they are not covered' do
+      cli = "#{base_cmd} ::TestApp::Literal#uncovered_string"
+      expect(Kernel.system(cli)).to be(false)
+    end
+
+    specify 'fails when some mutations are not covered' do
+      cli = "#{base_cmd} ::TestApp::Literal"
+      expect(Kernel.system(cli)).to be(false)
     end
   end
 
-  let(:base_cmd) { 'bundle exec mutant -I lib --require test_app --use rspec' }
+  context 'RSpec 2' do
+    let(:gemfile) { 'Gemfile.rspec2' }
 
-  specify 'it allows to kill mutations' do
-    expect(Kernel.system("#{base_cmd} ::TestApp::Literal#string")).to be(true)
+    it_behaves_like 'rspec integration'
   end
 
-  specify 'it allows to exclude mutations' do
-    cli = <<-CMD.split("\n").join(' ')
-      #{base_cmd}
-      ::TestApp::Literal#string
-      ::TestApp::Literal#uncovered_string
-        --ignore-subject ::TestApp::Literal#uncovered_string
-    CMD
-    expect(Kernel.system(cli)).to be(true)
-  end
+  context 'Rspec 3' do
+    let(:gemfile) { 'Gemfile.rspec3' }
 
-  specify 'fails to kill mutations when they are not covered' do
-    cli = "#{base_cmd} ::TestApp::Literal#uncovered_string"
-    expect(Kernel.system(cli)).to be(false)
-  end
-
-  specify 'fails when some mutations are not covered' do
-    cli = "#{base_cmd} ::TestApp::Literal"
-    expect(Kernel.system(cli)).to be(false)
+    it_behaves_like 'rspec integration'
   end
 end
