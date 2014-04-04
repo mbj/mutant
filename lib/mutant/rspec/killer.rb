69dc9e9e0e5bbd9129a323c9ce09a74a03050297
Markus Schirp <mbj@schirp-dso.com>
2014-04-04 16:24:13 +0000

    Use positive predicate

--- lib/mutant/rspec/killer.rb
+++ lib/mutant/rspec/killer.rb
@@ -20,7 +20,7 @@ module Mutant
       def run
         mutation.insert
 
-        unless example_groups.any?
+        if example_groups.empty?
           $stderr.puts("No rspec example groups found for: #{match_prefixes.join(', ')}")
           return false
         end
