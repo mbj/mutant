7eed61c1170e1678d15a76a41dac2471e499cdb6
Markus Schirp <mbj@schirp-dso.com>
2014-04-04 12:38:06 +0000

    Add require highjacker

---
+++ lib/mutant/require_highjack.rb
@@ -0,0 +1,66 @@
+module Mutant
+  # Require highjack
+  class RequireHighjack
+    include Concord.new(:target, :callback)
+
+    # Return original method
+    #
+    # @return [#call]
+    #
+    # @api private
+    #
+    attr_reader :original
+
+    # Run block with highjacked require
+    #
+    # @return [self]
+    #
+    # @api pivate
+    #
+    def run
+      infect
+      yield
+      self
+    ensure
+      desinfect
+    end
+
+  private
+
+    # Infect kernel with highjack
+    #
+    # @return [self]
+    #
+    # @api pivate
+    #
+    def infect
+      callback = @callback
+      @original = target.method(:require)
+      target.module_eval do
+        undef :require
+        define_method(:require) do |logical_name|
+          callback.call(logical_name)
+        end
+        module_function :require
+      end
+    end
+
+    # Imperfectly desinfect kernel from highjack
+    #
+    # @return [self]
+    #
+    # @api private
+    #
+    def desinfect
+      original = @original
+      target.module_eval do
+        undef :require
+        define_method(:require) do |logical_name|
+          original.call(logical_name)
+        end
+        module_function :require
+      end
+    end
+
+  end # RequireHighjack
+end # Mutant
---
+++ spec/unit/mutant/require_highjack_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe Mutant::RequireHighjack do
+  let(:object) { described_class.new(target, highjacked_calls.method(:push)) }
+
+  let(:highjacked_calls) { [] }
+  let(:require_calls)    { [] }
+
+  let(:target) do
+    require_calls = self.require_calls
+    Module.new do
+      define_method(:require) do |logical_name|
+        require_calls << logical_name
+      end
+      module_function :require
+    end
+  end
+
+  describe '#run' do
+    let(:block)        { -> { }                 }
+    let(:logical_name) { double('Logical Name') }
+
+    subject do
+      object.run(&block)
+    end
+
+    context 'require calls before run' do
+      it 'does not highjack anything' do
+        target.require(logical_name)
+        expect(require_calls).to eql([logical_name])
+        expect(highjacked_calls).to eql([])
+      end
+    end
+
+    context 'require calls during run' do
+      let(:block) { -> { target.require(logical_name) } }
+
+      it 'does highjack the calls' do
+        expect { subject }.to change { highjacked_calls }.from([]).to([logical_name])
+        expect(require_calls).to eql([])
+      end
+    end
+
+    context 'require calls after run' do
+
+      it 'does not the calls anything' do
+        subject
+        target.require(logical_name)
+        expect(require_calls).to eql([logical_name])
+        expect(highjacked_calls).to eql([])
+      end
+    end
+  end
+end
