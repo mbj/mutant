58fdd4f9b7b880bee9b0d9366eaaad9ca763eb8c
Markus Schirp <mbj@schirp-dso.com>
2014-04-22 17:37:21 +0000

    Remove expectation for invalid AST mutation

--- spec/unit/mutant/mutator/node/send_spec.rb
+++ spec/unit/mutant/mutator/node/send_spec.rb
@@ -63,7 +63,6 @@ describe Mutant::Mutator, 'send' do
 
     let(:mutations) do
       mutations = []
-      mutations << 'foo ||= expression'
       mutations << 'self.foo ||= nil'
       mutations << 'nil.foo ||= expression'
       mutations << 'nil'
