58ff6ee5d56624146cbecb7d1f194658b96760c9
Markus Schirp <mbj@schirp-dso.com>
2014-05-27 16:53:32 +0000

    Use correct metadata for test app

--- test_app/spec/unit/test_app/literal/command_spec.rb
+++ test_app/spec/unit/test_app/literal/command_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe TestApp::Literal, '#string' do
+RSpec.describe TestApp::Literal, '#command' do
   subject { object.command(double) }
 
   let(:object) { described_class.new }
