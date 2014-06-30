7e176907149fb69def7dde7044f5b508ff261706
Markus Schirp <mbj@schirp-dso.com>
2014-06-30 12:39:18 +0000

    Fix namespace matches result ordering to be deterministic

--- spec/unit/mutant/matcher/namespace_spec.rb
+++ spec/unit/mutant/matcher/namespace_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Mutant::Matcher::Namespace do
-  let(:object) { described_class.new(cache, Mutant::Expression.parse('TestApp::Literal')) }
+  let(:object) { described_class.new(cache, Mutant::Expression.parse('TestApp*')) }
   let(:yields) { []                                             }
 
   let(:cache) { Mutant::Cache.new }
@@ -10,15 +10,18 @@ describe Mutant::Matcher::Namespace do
 
   describe '#each' do
 
-    let(:singleton_a) { double('SingletonA', name: 'TestApp::Literal')      }
-    let(:singleton_b) { double('SingletonB', name: 'TestApp::Foo')          }
-    let(:singleton_c) { double('SingletonC', name: 'TestApp::LiteralOther') }
-    let(:subject_a)   { double('SubjectA')                                  }
-    let(:subject_b)   { double('SubjectB')                                  }
+    let(:singleton_a) { double('SingletonA', name: 'TestApp::Literal') }
+    let(:singleton_b) { double('SingletonB', name: 'TestApp::Foo')     }
+    let(:singleton_c) { double('SingletonC', name: 'TestAppOther')     }
+    let(:subject_a)   { double('SubjectA')                             }
+    let(:subject_b)   { double('SubjectB')                             }
 
     before do
       allow(Mutant::Matcher::Methods::Singleton).to receive(:new).with(cache, singleton_a).and_return([subject_a])
-      allow(Mutant::Matcher::Methods::Instance).to receive(:new).with(cache, singleton_a).and_return([subject_b])
+      allow(Mutant::Matcher::Methods::Instance).to receive(:new).with(cache, singleton_a).and_return([])
+
+      allow(Mutant::Matcher::Methods::Singleton).to receive(:new).with(cache, singleton_b).and_return([subject_b])
+      allow(Mutant::Matcher::Methods::Instance).to receive(:new).with(cache, singleton_b).and_return([])
 
       allow(ObjectSpace).to receive(:each_object).with(Module).and_return([singleton_a, singleton_b, singleton_c])
     end
@@ -34,7 +37,7 @@ describe Mutant::Matcher::Namespace do
     end
 
     it 'should yield subjects' do
-      expect { subject }.to change { yields }.from([]).to([subject_a, subject_b])
+      expect { subject }.to change { yields }.from([]).to([subject_b, subject_a])
     end
   end
 end
