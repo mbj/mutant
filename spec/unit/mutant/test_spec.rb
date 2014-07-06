89ea14b5a1c03e0231cde4145a9303045807c27e
Markus Schirp <mbj@schirp-dso.com>
2014-07-06 14:39:06 +0000

    Add specification of Mutant::Test

---
+++ spec/unit/mutant/test_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Mutant::Test do
+  let(:object) { described_class.new(integration, expression) }
+
+  let(:integration) { double('Integration', name: 'test-integration') }
+  let(:expression)  { double('Expression', syntax: 'test-syntax') }
+
+  describe '#identification' do
+    subject { object.identification }
+
+    it { should eql('test-integration:test-syntax') }
+  end
+
+  describe '#run' do
+    subject { object.run }
+
+    let(:report) { double('Report') }
+
+    it 'runs test via integration' do
+      expect(integration).to receive(:run).with(object).and_return(report)
+      expect(subject).to be(report)
+    end
+  end
+end
