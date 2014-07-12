47bf498d4c22c5ebf9124c6b26a3f8e5be2dc24a
Markus Schirp <mbj@schirp-dso.com>
2014-07-12 21:01:51 +0000

    Move spec subject to null integration

--- spec/unit/mutant/integration_spec.rb
+++ spec/unit/mutant/integration_spec.rb
@@ -8,15 +8,21 @@ describe Mutant::Integration do
 
   let(:object) { class_under_test.new }
 
-  describe '#all_tests' do
-    subject { object.all_tests }
-
-    it { should eql([]) }
-    it_should_behave_like 'an idempotent method'
-  end
-
   describe '#setup' do
     subject { object.setup }
     it_should_behave_like 'a command method'
+  end
+end
+
+describe Mutant::Integration::Null do
+
+  let(:object) { described_class.new }
+
+  describe '#setup' do
+    subject { object.all_tests }
+
+    it { should eql([]) }
+
+    it_should_behave_like 'an idempotent method'
   end
 end
