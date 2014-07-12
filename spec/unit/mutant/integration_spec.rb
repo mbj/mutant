13516a0cc7a2a9144456c19cdea75174b1c009f1
Markus Schirp <mbj@schirp-dso.com>
2014-07-12 20:53:57 +0000

    Add test for Mutant::Integration#all_tests

--- spec/unit/mutant/integration_spec.rb
+++ spec/unit/mutant/integration_spec.rb
@@ -8,6 +8,13 @@ describe Mutant::Integration do
 
   let(:object) { class_under_test.new }
 
+  describe '#all_tests' do
+    subject { object.all_tests }
+
+    it { should eql([]) }
+    it_should_behave_like 'an idempotent method'
+  end
+
   describe '#setup' do
     subject { object.setup }
     it_should_behave_like 'a command method'
