0f7fb665cb967a4124f1f2178b79d157f6960d4c
Markus Schirp <mbj@schirp-dso.com>
2014-11-17 19:30:58 +0000

    Fix mutator registry use from specs

--- spec/integration/mutant/test_mutator_handles_types_spec.rb
+++ spec/integration/mutant/test_mutator_handles_types_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe do
 
   specify 'mutant should not crash for any node parser can generate' do
     Mutant::AST::Types::ALL.each do |type|
-      Mutant::Mutator::Registry.lookup(s(type))
+      Mutant::Mutator::REGISTRY.lookup(s(type))
     end
   end
 end
