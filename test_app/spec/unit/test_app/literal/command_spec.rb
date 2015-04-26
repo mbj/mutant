b3a4735bba6c1065edba2d93f553bd667970d151
Keenan Brock <keenan@thebrocks.net>
2015-04-26 13:35:11 -0400

    Add single spec for all literal tests

--- test_app/spec/unit/test_app/literal/command_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe TestApp::Literal, '#command' do
-  subject { object.command(double) }
-
-  let(:object) { described_class.new }
-
-  it { should be(object) }
-end
---
+++ test_app/spec/unit/test_app/literal_spec.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+RSpec.describe TestApp::Literal do
+
+  describe '#command' do
+    subject { object.command(double) }
+
+    let(:object) { described_class.new }
+
+    it { should be(object) }
+  end
+
+  describe '#string' do
+    subject { object.string }
+
+    let(:object) { described_class.new }
+
+    it { should eql('string') }
+  end
+end
