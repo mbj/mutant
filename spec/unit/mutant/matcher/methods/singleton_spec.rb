0495f59bcd55194d547a62c33841f9865c1a93cb
Keenan Brock <keenan@thebrocks.net>
2015-04-27 21:26:31 -0400

    Redefine class in singleton matcher spec
    - rspec describes are not classes, so the class was defined globally
    - this caused warnings for other matchers

--- spec/unit/mutant/matcher/methods/singleton_spec.rb
+++ spec/unit/mutant/matcher/methods/singleton_spec.rb
@@ -1,33 +1,35 @@
 RSpec.describe Mutant::Matcher::Methods::Singleton, '#each' do
-  let(:object) { described_class.new(env, Foo) }
+  let(:object) { described_class.new(env, class_under_test) }
   let(:env)    { Fixtures::TEST_ENV            }
 
   subject { object.each { |matcher| yields << matcher } }
 
   let(:yields) { [] }
 
-  module Bar
-    def method_d
+  let(:class_under_test) do
+    parent = Module.new do
+      def method_d
+      end
+
+      def method_e
+      end
     end
 
-    def method_e
+    Class.new do
+      extend parent
+
+      def self.method_a
+      end
+
+      def self.method_b
+      end
+      class << self; protected :method_b; end
+
+      def self.method_c
+      end
+      private_class_method :method_c
+
     end
-  end
-
-  class Foo
-    extend Bar
-
-    def self.method_a
-    end
-
-    def self.method_b
-    end
-    class << self; protected :method_b; end
-
-    def self.method_c
-    end
-    private_class_method :method_c
-
   end
 
   let(:subject_a) { double('Subject A') }
@@ -39,11 +41,11 @@ RSpec.describe Mutant::Matcher::Methods::Singleton, '#each' do
   before do
     matcher = Mutant::Matcher::Method::Singleton
     allow(matcher).to receive(:new)
-      .with(env, Foo, Foo.method(:method_a)).and_return([subject_a])
+      .with(env, class_under_test, class_under_test.method(:method_a)).and_return([subject_a])
     allow(matcher).to receive(:new)
-      .with(env, Foo, Foo.method(:method_b)).and_return([subject_b])
+      .with(env, class_under_test, class_under_test.method(:method_b)).and_return([subject_b])
     allow(matcher).to receive(:new)
-      .with(env, Foo, Foo.method(:method_c)).and_return([subject_c])
+      .with(env, class_under_test, class_under_test.method(:method_c)).and_return([subject_c])
   end
 
   it 'should yield expected subjects' do
