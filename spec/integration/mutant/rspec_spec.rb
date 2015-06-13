d6b3932ca9d3a2b46a3a65de9f54c90e4163a585
Markus Schirp <mbj@schirp-dso.com>
2015-06-13 04:00:20 +0000

    Add support for rspec-3.3
    [fix #345]

--- spec/integration/mutant/rspec_spec.rb
+++ spec/integration/mutant/rspec_spec.rb
@@ -2,21 +2,11 @@ RSpec.describe 'rspec integration', mutant: false do
 
   let(:base_cmd) { 'bundle exec mutant -I lib --require test_app --use rspec' }
 
-  context 'RSpec 3.0' do
-    let(:gemfile) { 'Gemfile.rspec3.0' }
+  %w[3.0 3.1 3.2 3.3].each do |version|
+    context "RSpec #{version}" do
+      let(:gemfile) { "Gemfile.rspec#{version}" }
 
-    it_behaves_like 'framework integration'
-  end
-
-  context 'RSpec 3.1' do
-    let(:gemfile) { 'Gemfile.rspec3.1' }
-
-    it_behaves_like 'framework integration'
-  end
-
-  context 'RSpec 3.2' do
-    let(:gemfile) { 'Gemfile.rspec3.2' }
-
-    it_behaves_like 'framework integration'
+      it_behaves_like 'framework integration'
+    end
   end
 end
