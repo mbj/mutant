d96ecba748449d9f55fb0c6661fb99a7952ef37d
Markus Schirp <mbj@schirp-dso.com>
2015-08-14 16:59:31 +0000

    Remove invalid mutation super(...) to super

--- meta/super.rb
+++ meta/super.rb
@@ -9,15 +9,12 @@ Mutant::Meta::Example.add do
   source 'super()'
 
   singleton_mutations
-  # this is zsuper a totally different node than super()
-  mutation 'super'
 end
 
 Mutant::Meta::Example.add do
   source 'super(foo, bar)'
 
   singleton_mutations
-  mutation 'super'
   mutation 'super()'
   mutation 'super(foo)'
   mutation 'super(bar)'
