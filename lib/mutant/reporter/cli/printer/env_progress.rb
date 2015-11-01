722aa7664044b12ebd1a77ae25067740ebe188fa
Markus Schirp <mbj@schirp-dso.com>
2015-11-01 20:46:19 +0000

    Add more metrics to env progress reporter

--- lib/mutant/reporter/cli/printer/env_progress.rb
+++ lib/mutant/reporter/cli/printer/env_progress.rb
@@ -10,33 +10,50 @@ module Mutant
             :amount_mutations,
             :amount_mutations_alive,
             :amount_mutations_killed,
+            :amount_mutation_results,
             :runtime,
             :killtime,
             :overhead,
             :env
           )
 
+          # rubocop:disable SpaceInsideBrackets
+          FORMATS = IceNine.deep_freeze([
+            [:info,   'Subjects:        %s',      :amount_subjects          ],
+            [:info,   'Mutations:       %s',      :amount_mutations         ],
+            [:info,   'Results:         %s',      :amount_mutation_results  ],
+            [:info,   'Kills:           %s',      :amount_mutations_killed  ],
+            [:info,   'Alive:           %s',      :amount_mutations_alive   ],
+            [:info,   'Runtime:         %0.2fs',  :runtime                  ],
+            [:info,   'Killtime:        %0.2fs',  :killtime                 ],
+            [:info,   'Overhead:        %0.2f%%', :overhead_percent         ],
+            [:info,   'Mutations/s:     %0.2f',   :mutations_per_second     ],
+            [:status, 'Coverage:        %0.2f%%', :coverage_percent         ],
+            [:status, 'Expected:        %0.2f%%', :expected_coverage_percent]
+          ])
+
           # Run printer
           #
           # @return [undefined]
           #
-          # rubocop:disable AbcSize
-          #
           # @api private
           def run
             visit(Config, env.config)
-            info 'Subjects:        %s',        amount_subjects
-            info 'Mutations:       %s',        amount_mutations
-            info 'Kills:           %s',        amount_mutations_killed
-            info 'Alive:           %s',        amount_mutations_alive
-            info 'Runtime:         %0.2fs',    runtime
-            info 'Killtime:        %0.2fs',    killtime
-            info 'Overhead:        %0.2f%%',   overhead_percent
-            status 'Coverage:        %0.2f%%', coverage_percent
-            status 'Expected:        %0.2f%%', (env.config.expected_coverage * 100)
+            FORMATS.each do |report, format, value|
+              __send__(report, format, __send__(value))
+            end
           end
 
         private
+
+          # Mutations processed per second
+          #
+          # @return [Float]
+          #
+          # @api private
+          def mutations_per_second
+            amount_mutation_results / runtime
+          end
 
           # Coverage in percent
           #
@@ -45,6 +62,15 @@ module Mutant
           # @api private
           def coverage_percent
             coverage * 100
+          end
+
+          # Expected coverage in percent
+          #
+          # @return [Float]
+          #
+          # @api private
+          def expected_coverage_percent
+            env.config.expected_coverage * 100
           end
 
           # Overhead in percent
--- spec/unit/mutant/reporter/cli/printer/status_spec.rb
+++ spec/unit/mutant/reporter/cli/printer/status_spec.rb
@@ -17,11 +17,13 @@ RSpec.describe Mutant::Reporter::CLI::Printer::Status do
         Requires:        []
         Subjects:        1
         Mutations:       2
+        Results:         0
         Kills:           0
         Alive:           0
         Runtime:         4.00s
         Killtime:        0.00s
         Overhead:        Inf%
+        Mutations/s:     0.00
         Coverage:        100.00%
         Expected:        100.00%
         Active subjects: 0
@@ -40,11 +42,13 @@ RSpec.describe Mutant::Reporter::CLI::Printer::Status do
           Requires:        []
           Subjects:        1
           Mutations:       2
+          Results:         0
           Kills:           0
           Alive:           0
           Runtime:         4.00s
           Killtime:        0.00s
           Overhead:        Inf%
+          Mutations/s:     0.00
           Coverage:        100.00%
           Expected:        10.00%
           Active subjects: 0
@@ -66,11 +70,13 @@ RSpec.describe Mutant::Reporter::CLI::Printer::Status do
           Requires:        []
           Subjects:        1
           Mutations:       2
+          Results:         2
           Kills:           2
           Alive:           0
           Runtime:         4.00s
           Killtime:        2.00s
           Overhead:        100.00%
+          Mutations/s:     0.50
           Coverage:        100.00%
           Expected:        100.00%
           Active subjects: 0
@@ -93,11 +99,13 @@ RSpec.describe Mutant::Reporter::CLI::Printer::Status do
             Requires:        []
             Subjects:        1
             Mutations:       2
+            Results:         2
             Kills:           1
             Alive:           1
             Runtime:         4.00s
             Killtime:        2.00s
             Overhead:        100.00%
+            Mutations/s:     0.50
             Coverage:        50.00%
             Expected:        100.00%
             Active Jobs:
@@ -121,11 +129,13 @@ RSpec.describe Mutant::Reporter::CLI::Printer::Status do
             Requires:        []
             Subjects:        1
             Mutations:       2
+            Results:         2
             Kills:           2
             Alive:           0
             Runtime:         4.00s
             Killtime:        2.00s
             Overhead:        100.00%
+            Mutations/s:     0.50
             Coverage:        100.00%
             Expected:        100.00%
             Active Jobs:
