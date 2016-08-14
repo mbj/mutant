1037614d4128ff9818a9723fc82edb06e9e3ee47
John Backus <johncbackus@gmail.com>
2016-08-14 13:52:04 -0700

    Remove Regexp::Generic

--- lib/mutant/mutator/node/regexp.rb
+++ lib/mutant/mutator/node/regexp.rb
@@ -2,17 +2,6 @@ module Mutant
   class Mutator
     class Node
       module Regexp
-        # Generic regexp mutator
-        class Generic < Node
-          handle(*(AST::Types::REGEXP - %i[regexp_root_expression regexp_bol_anchor]))
-
-          # Noop dispatch
-          #
-          # @return [undefined]
-          def dispatch
-          end
-        end # Generic
-
         # Mutator for root expression regexp wrapper
         class RootExpression < Node
           handle(:regexp_root_expression)
