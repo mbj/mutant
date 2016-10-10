47ad4806c29f2f19885eff5883ed8af065aadc1b
John Backus <johncbackus@gmail.com>
2016-10-10 02:01:14 -0700

    Fix invalid lvar mutations

--- lib/mutant/mutator/node/named_value/variable_assignment.rb
+++ lib/mutant/mutator/node/named_value/variable_assignment.rb
@@ -27,6 +27,8 @@ module Mutant
           #
           # @return [undefined]
           def dispatch
+            return if lvar_used?
+
             emit_singletons
             mutate_name
             emit_value_mutations if value # op asgn!
@@ -42,6 +44,66 @@ module Mutant
               emit_name(:"#{prefix}#{name}")
             end
           end
+
+          # Test if a local variable is used within siblings
+          #
+          # @return [Boolean]
+          def lvar_used?
+            n_lvasgn?(node) && lvar_read?(name)
+          end
+
+          # Test if a local variable of a given name is read within siblings
+          #
+          # @param name [Symbol] name of lvar
+          #
+          # @return [Boolean]
+          def lvar_read?(name)
+            lvar_reads_in_scope.include?(s(:lvar, name))
+          end
+
+          # Local variables defined within sibling nodes defined after the current node
+          #
+          # @return [Array<Parser::AST::Node]
+          def lvar_reads_in_scope
+            recurse_siblings.select(&method(:n_lvar?))
+          end
+          memoize :lvar_reads_in_scope
+
+          # Recurse through siblings defined after the current node
+          #
+          # @yield [Parser::AST::Node] node matching block provided
+          #
+          # @return [undefined]
+          def recurse_siblings(&block)
+            return to_enum(__method__) unless block_given?
+
+            following_siblings.each { |sibling| recurse_node(sibling, &block) }
+          end
+
+          # Descend recursively into node
+          #
+          # @param node [Object]
+          #
+          # @yield [node] node matching block provided
+          #
+          # @return [undefinefd]
+          def recurse_node(node, &block)
+            return unless node.instance_of?(::Parser::AST::Node)
+
+            yield(node)
+
+            node.children.each { |child| recurse_node(child, &block) }
+          end
+
+          # Children of the parent defined after the current node
+          #
+          # @return [Array<Parser::AST::Node>]
+          def following_siblings
+            return [] unless parent
+
+            parent.children.slice_when { |node| node.equal?(self) }.to_a.last
+          end
+          memoize :following_siblings
 
         end # VariableAssignment
       end # NamedValue
--- meta/lvar.rb
+++ meta/lvar.rb
@@ -4,11 +4,4 @@ Mutant::Meta::Example.add :lvar do
   mutation 'a = nil; nil'
   mutation 'a = nil; self'
   mutation 'a = nil'
-  # TODO: fix invalid AST
-  #   These ASTs are not valid and should NOT be emitted
-  #   Mutations of lvarasgn need to be special cased to avoid this.
-  mutation s(:begin, s(:lvasgn, :a__mutant__, s(:nil)), s(:lvar, :a))
-  mutation s(:begin, s(:nil), s(:lvar, :a))
-  mutation s(:begin, s(:self), s(:lvar, :a))
-  mutation s(:lvar, :a)
 end
