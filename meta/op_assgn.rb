8e1864ca75d0d586c6a42cf192b5124e61366d03
Daniel Gollahon <daniel@blockscore.com>
2016-10-10 23:18:12 -0700

    Add existing  mutations for op_asgn

--- meta/op_assgn.rb
+++ meta/op_assgn.rb
@@ -13,3 +13,21 @@ Mutant::Meta::Example.add :op_asgn do
   #   This should not get emitted as invalid AST with valid unparsed source
   mutation s(:op_asgn, s(:ivar, :@a), :+, s(:int, 1))
 end
+
+Mutant::Meta::Example.add :op_asgn do
+  source 'a *= b'
+
+  singleton_mutations
+  mutation 'a__mutant__ *= b'
+  mutation 'a *= nil'
+  mutation 'a *= self'
+end
+
+Mutant::Meta::Example.add :op_asgn do
+  source '@a <<= b'
+
+  singleton_mutations
+  mutation '@a__mutant__ <<= b'
+  mutation '@a <<= nil'
+  mutation '@a <<= self'
+end
