99fcf508469aa626eb344b7eea69375241d7597b
Krzysztof Rybka <krzysztof.rybka@gmail.com>
2017-05-08 21:54:29 +0200

    Add support for rspec-3.6

--- spec/integration/mutant/rspec_spec.rb
+++ spec/integration/mutant/rspec_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe 'rspec integration', mutant: false do
 
   let(:base_cmd) { 'bundle exec mutant -I lib --require test_app --use rspec' }
 
-  %w[3.4 3.5].each do |version|
+  %w[3.4 3.5 3.6].each do |version|
     context "RSpec #{version}" do
       let(:gemfile) { "Gemfile.rspec#{version}" }
 
