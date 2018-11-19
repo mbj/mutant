2d6c7667501485d5a8822aee68593b2f89a787a5
Markus Schirp <mbj@schirp-dso.com>
2018-11-19 12:04:00 +0000

    Remove old ruby references

--- spec/integration/mutant/corpus_spec.rb
+++ spec/integration/mutant/corpus_spec.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Mutant on ruby corpus', mutant: false do
-
-  before do
-    skip 'Corpus test is deactivated on < 2.1' if RUBY_VERSION < '2.1'
-    skip 'Corpus test is deactivated on RBX' if RUBY_ENGINE.eql?('rbx')
-  end
-
   MutantSpec::Corpus::Project::ALL.select(&:mutation_generation).each do |project|
     specify "#{project.name} does not fail on mutation generation" do
       project.verify_mutation_generation
