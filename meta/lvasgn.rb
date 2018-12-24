97599aaad0dcc7cd745cc5445e3b21d200a2d699
Markus Schirp <mbj@schirp-dso.com>
2018-12-24 09:19:32 +0000

    Fix emit of naked splat
    [Fix #724]

--- meta/lvasgn.rb
+++ meta/lvasgn.rb
@@ -8,3 +8,18 @@ Mutant::Meta::Example.add :lvasgn do
   mutation 'a = false'
   mutation 'a = nil'
 end
+
+Mutant::Meta::Example.add :array, :lvasgn do
+  source 'a = *b'
+
+  singleton_mutations
+  mutation 'a__mutant__ = *b'
+  mutation 'a = nil'
+  mutation 'a = self'
+  mutation 'a = []'
+  mutation 'a = [nil]'
+  mutation 'a = [self]'
+  mutation 'a = [*self]'
+  mutation 'a = [*nil]'
+  mutation 'a = [b]'
+end
