7699e84627c653b91557a614c59533c2cd91727f
Markus Schirp <mbj@schirp-dso.com>
2018-12-31 15:20:38 +0000

    Refactor fork isolation to abstract pipe pair better
    * This allows adding more pipes without cluttering up the lvar space of
      parent and child

--- lib/mutant/isolation/fork.rb
+++ lib/mutant/isolation/fork.rb
@@ -9,7 +9,7 @@ module Mutant
         Anima.new(:devnull, :io, :marshal, :process, :stderr, :stdout)
       )
 
-      ATTRIBUTES = (anima.attribute_names + %i[block reader writer]).freeze
+      ATTRIBUTES = (anima.attribute_names + %i[block result_pipe]).freeze
 
       # Unsucessful result as child exited nonzero
       class ChildError < Result
@@ -20,6 +20,36 @@ module Mutant
       class ForkError < Result
         include Equalizer.new
       end # ForkError
+
+      # Pipe abstraction
+      class Pipe
+        include Adamantium::Flat, Anima.new(:reader, :writer)
+
+        # Run block with pipe in binmode
+        #
+        # @return [undefined]
+        def self.with(io)
+          io.pipe(binmode: true) do |(reader, writer)|
+            yield new(reader: reader, writer: writer)
+          end
+        end
+
+        # Child writer end of the pipe
+        #
+        # @return [IO]
+        def child
+          reader.close
+          writer
+        end
+
+        # Parent reader end of the pipe
+        #
+        # @return [IO]
+        def parent
+          writer.close
+          reader
+        end
+      end # Pipe
 
       # ignore :reek:InstanceVariableAssumption
       class Parent
@@ -48,7 +78,9 @@ module Mutant
         #
         # @return [Integer]
         def start_child
-          process.fork { Child.call(to_h) }
+          process.fork do
+            Child.call(to_h.merge(result_pipe: result_pipe.child))
+          end
         end
 
         # Read child result
@@ -57,9 +89,7 @@ module Mutant
         #
         # @return [undefined]
         def read_child_result(pid)
-          writer.close
-
-          add_result(Result::Success.new(marshal.load(reader)))
+          add_result(Result::Success.new(marshal.load(result_pipe.parent)))
         rescue ArgumentError, EOFError => exception
           add_result(Result::Exception.new(exception))
         ensure
@@ -96,9 +126,8 @@ module Mutant
         #
         # @return [undefined]
         def call
-          reader.close
-          writer.syswrite(marshal.dump(result(&block)))
-          writer.close
+          result_pipe.syswrite(marshal.dump(compute(&block)))
+          result_pipe.close
         end
 
       private
@@ -106,7 +135,7 @@ module Mutant
         # The block result computed under silencing
         #
         # @return [Object]
-        def result
+        def compute
           devnull.call do |null|
             stderr.reopen(null)
             stdout.reopen(null)
@@ -122,8 +151,8 @@ module Mutant
       # @return [Result]
       #   execution result
       def call(&block)
-        io.pipe(binmode: true) do |(reader, writer)|
-          Parent.call(to_h.merge(block: block, reader: reader, writer: writer))
+        Pipe.with(io) do |result|
+          Parent.call(to_h.merge(block: block, result_pipe: result))
         end
       end
     end # Fork
