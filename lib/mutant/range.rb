2ebccf2a2792ff18f0dc4853d39f6b47cf7386e5
Markus Schirp <mbj@schirp-dso.com>
2019-03-07 12:21:04 +0000

    Add Mutant::Range.overlap?

---
+++ lib/mutant/range.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Mutant
+  module Range
+    # Test if two ranges overlap
+    #
+    # @param [Range] left
+    # @param [Range] right
+    #
+    # @return [Boolean]
+    def self.overlap?(left, right)
+      left.end >= right.begin && right.end >= left.begin
+    end
+  end # Range
+end # end
