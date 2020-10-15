f37cc0e05bd678d39cc73d57f4d1c707eda0743c
Markus Schirp <mbj@schirp-dso.com>
2020-10-15 20:07:52 +0000

    Add primitive devloop.sh

---
+++ scripts/devloop.sh
@@ -0,0 +1,4 @@
+while inotifywait **/*.rb Gemfile mutant.gemspec; do
+  bundle exec rspec spec/unit -fd --fail-fast --order default \
+    && bundle exec ./mutant.sh --since master -- 'Mutant*'
+done
