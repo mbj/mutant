24f6b6bf79e3ddd8cea6b31e758e5bb6259f8fd0
Markus Schirp <mbj@schirp-dso.com>
2020-11-04 04:57:23 +0000

    Change Mutant::World to separate file

--- lib/mutant/config.rb
+++ lib/mutant/config.rb
@@ -1,55 +1,6 @@
 # frozen_string_literal: true
 
 module Mutant
-  # The outer world IO objects mutant does interact with
-  class World
-    include Adamantium::Flat, Anima.new(
-      :condition_variable,
-      :gem,
-      :gem_method,
-      :io,
-      :json,
-      :kernel,
-      :load_path,
-      :marshal,
-      :mutex,
-      :object_space,
-      :open3,
-      :pathname,
-      :process,
-      :stderr,
-      :stdout,
-      :thread,
-      :warnings
-    )
-
-    INSPECT = '#<Mutant::World>'
-
-    private_constant(*constants(false))
-
-    # Object inspection
-    #
-    # @return [String]
-    def inspect
-      INSPECT
-    end
-
-    # Capture stdout of a command
-    #
-    # @param [Array<String>] command
-    #
-    # @return [Either<String,String>]
-    def capture_stdout(command)
-      stdout, status = open3.capture2(*command, binmode: true)
-
-      if status.success?
-        Either::Right.new(stdout)
-      else
-        Either::Left.new("Command #{command} failed!")
-      end
-    end
-  end # World
-
   # Standalone configuration of a mutant execution.
   #
   # Does not reference any "external" volatile state. The configuration applied
