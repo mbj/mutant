5a7d4b2df90a88bf719aeea690c467b4c5bba9fd
Daniel Gollahon <daniel.gollahon@gmail.com>
2021-11-07 12:20:58 -0800

    Fix unicode property support
    * Original work in https://github.com/mbj/mutant/pull/1278
    * Adapted to reflect the unicode properties from the `regexp_parser`
      gem.
    * Regexp parser gem seems to not properly map ruby features for each
      release so subsetting via filtering against the ruby regexp parser.

--- lib/mutant/version.rb
+++ lib/mutant/version.rb
@@ -2,5 +2,5 @@
 
 module Mutant
   # Current mutant version
-  VERSION = '0.11.6'
+  VERSION = '0.11.7'
 end # Mutant
--- meta/regexp/regexp_latin_property.rb
+++ meta/regexp/regexp_latin_property.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Mutant::Meta::Example.add :regexp, :regexp_latin_property do
-  source('/p{Latin}/')
+  source('/p{latin}/')
 
   singleton_mutations
   regexp_mutations
