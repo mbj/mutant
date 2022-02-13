6db69de14088e3c339dff2ae0bca6723ed70df30
Markus Schirp <mbj@schirp-dso.com>
2022-02-13 23:41:39 +0000

    Refactor spec style towards project standard

--- spec/unit/mutant/ast/find_metaclass_containing_spec.rb
+++ spec/unit/mutant/ast/find_metaclass_containing_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Mutant::AST::FindMetaclassContaining do
     subject { described_class.call(ast, node) }
 
     let(:metaclass_node) { s(:sclass, s(:self), method_node) }
-    let(:method_node) { s(:def, 'test', s(:nil)) }
+    let(:method_node)    { s(:def, 'test', s(:nil))          }
 
-    let(:ast) { metaclass_node }
-    let(:node) { method_node }
+    let(:ast)  { metaclass_node }
+    let(:node) { method_node    }
 
     context 'when called without ast' do
       let(:ast) { nil }
@@ -24,21 +24,21 @@ RSpec.describe Mutant::AST::FindMetaclassContaining do
     context 'when called without node' do
       let(:node) { nil }
 
-      it { is_expected.to be nil }
+      specify { expect(subject).to be(nil) }
     end
 
     context 'when called with ast which contains a duplicate of the node' do
       let(:node) { s(:def, 'test', s(:nil)) }
 
       shared_examples_for 'unfooled by the duplicate node' do
-        it { is_expected.to be nil }
+        specify { expect(subject).to be(nil) }
 
         # if we changed method_node or node without altering the other to match,
         # the above example would provide a false positive. by ensuring node and
         # method_node are eq but not equal, we ensure the usefulness of the
         # above example.
-        it { expect(node).to eq(method_node) }
-        it { expect(node).not_to be method_node }
+        specify { expect(node).to eql(method_node) }
+        specify { expect(node).not_to be(method_node) }
       end
 
       it_behaves_like 'unfooled by the duplicate node'
@@ -51,18 +51,18 @@ RSpec.describe Mutant::AST::FindMetaclassContaining do
     end
 
     context 'when called with ast containing the node' do
-      it { is_expected.to be metaclass_node }
+      specify { expect(subject).to be(metaclass_node) }
 
       context 'inside a begin block' do
         let(:metaclass_node) { s(:sclass, s(:self), s(:begin, method_node)) }
 
-        it { is_expected.to be metaclass_node }
+        it { expect(subject).to be(metaclass_node) }
       end
 
       context 'inside a different class' do
         let(:metaclass_node) { s(:sclass, s(:self), s(:class, :MyClass, nil, method_node)) }
 
-        it { is_expected.to be nil }
+        it { expect(subject).to be(nil) }
       end
     end
   end
