1f7ba129a572b403fefaf0757c4a4af18e788679
Markus Schirp <mbj@schirp-dso.com>
2024-02-14 01:41:25 +0000

    Change to process warmup in parallel abstraction
    * This will benefit all future parallel clients, not just the mutation runner.

--- scripts/devloop.sh
+++ scripts/devloop.sh
@@ -1,5 +1,5 @@
 while inotifywait lib/**/*.rb meta/**/*.rb spec/**/*.rb Gemfile Gemfile.shared mutant.gemspec; do
-  bundle exec rspec spec/unit -fd --fail-fast --order default \
+  bundle exec rspec spec/unit -fd --fail-fast --order defined \
     && bundle exec mutant run --since main --fail-fast --zombie -- 'Mutant*' \
     && bundle exec rubocop
 done
