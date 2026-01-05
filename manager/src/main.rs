283fd00a31e07d3a3584dbbdfc724a9cf10b06c4
Markus Schirp <mbj@schirp-dso.com>
2026-01-05 13:05:21 +0000

    Add rerun support

--- manager/src/main.rs
+++ manager/src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser, Subcommand};
 use std::fs;
+use std::num::NonZeroU32;
 
 mod ruby;
 
@@ -18,6 +19,14 @@ enum Commands {
         #[arg(long, default_value = "host")]
         ruby_runtime: ruby::Runtime,
 
+        /// Run command multiple times
+        #[arg(long)]
+        rerun: Option<NonZeroU32>,
+
+        /// Concurrency for reruns (defaults to number of CPUs)
+        #[arg(long)]
+        jobs: Option<NonZeroU32>,
+
         #[command(subcommand)]
         action: ruby::Command,
     },
@@ -29,10 +38,25 @@ fn main() -> ruby::RunResult {
     match cli.command {
         Commands::Ruby {
             ruby_runtime,
+            rerun,
+            jobs,
             action,
         } => {
             prepare();
-            action.run(ruby_runtime)
+
+            let runs = rerun.map(|n| n.get()).unwrap_or(1);
+
+            let jobs = jobs.map(|n| n.get()).unwrap_or_else(|| {
+                std::thread::available_parallelism()
+                    .map(|parallelism| parallelism.get() as u32)
+                    .unwrap_or_else(|_| {
+                        log::warn!("Could not determine CPU count, defaulting to 1 job");
+                        1
+                    })
+            });
+
+            let rerun_config = ruby::RerunConfig { runs, jobs };
+            action.run(ruby_runtime, rerun_config)
         }
     }
 }
