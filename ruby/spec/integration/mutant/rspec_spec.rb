76bf1c1a762e7264ce31672fa3d74296c6df25f9
Paweł Pacana <pawel.pacana@gmail.com>
2026-04-29 18:02:35 +0200

    Follow structure of minitest integration
    One integration, one subdirectory with Gemfile versions. This would
    further allow nesting integration-specific tests under integration root.

--- ruby/spec/integration/mutant/rspec_spec.rb
+++ ruby/spec/integration/mutant/rspec_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'rspec integration', mutant: false do
 
   %w[3.8 3.9 3.10 3.11 3.12 3.13 4.0].each do |version|
     context "RSpec #{version}" do
-      let(:gemfile) { "rspec#{version}/Gemfile" }
+      let(:gemfile) { "rspec/Gemfile.rspec_#{version.tr('.', '_')}" }
 
       it_behaves_like 'framework integration'
 
